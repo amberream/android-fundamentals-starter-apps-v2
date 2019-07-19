0c98117baeaed54d0512969e52877a6b481b30ac
amberream <adream114@gmail.com>
2019-07-18 22:31:19 -0400

    Android fundamentals 08.2: The alarm manager Task 2

--- StandUp/app/src/main/java/com/amberream/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/amberream/standup/MainActivity.java
@@ -1,5 +1,13 @@
 package com.amberream.standup;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -8,7 +16,12 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    // for Android 8 (api 27) you need a notification channel
+    private static final String NOTIFICATION_CHANNEL = "notificationChannel";
+    private static final int NOTIFICATION_ID = 0;
+
     private ToggleButton alarmToggle;
+    NotificationManager mNotificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,49 @@ public class MainActivity extends AppCompatActivity {
                 if (isChecked)
                 {
                     toastMessage = getString(R.string.standup_alarm_on);
+                    deliverNotification(MainActivity.this);
                 }
                 else
                 {
                     toastMessage = getString(R.string.standup_alarm_off);
+                    mNotificationManager.cancelAll();
                 }
                 Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT);
             }
         });
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel(); // for Android 8 (api 26)
     }
+
+    private void createNotificationChannel() {
+        // notification channels are available/required for android 8 (api 26)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL, "Stand Up Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context)
+    {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL);
+        builder.setSmallIcon(R.drawable.ic_standup);
+        builder.setContentTitle("Stand Up Alert");
+        builder.setContentText("You should stand up and walk around now");
+        builder.setContentIntent(pendingIntent);
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setAutoCancel(true);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
 }
